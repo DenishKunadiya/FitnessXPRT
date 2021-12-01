018089bf7b9ec74efcaaa8cf071dd485788c8b53
denish kunadiya <denish.login@gmail.com>
2021-12-01 18:36:24 +0530

    blog page static is complete

--- src/components/blog-post/BlogPost.component.jsx
+++ src/components/blog-post/BlogPost.component.jsx
@@ -5,42 +5,73 @@ import "./blogpost.styles.css";
 export default class BlogPost extends Component {
   render() {
     return (
-      <div>
-        <div className="container mt-5">
-          <div className="row">
-            <div className="col-6 col-md-6 col-lg-6">
-              <div className="card mb-3">
-                <img src={blogpost} alt="Profile" />
-                <div className="container blog-post-footer">
-                  <div className="row">
-                    <div className="col-md-2">dsd</div>
-                    <div className="col-md-2">sdf</div>
-                    <div className="col-md-8 text-end">fsdfd</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-6 col-md-6 col-lg-6">
-              <div className="card mb-3">
-                <img src={blogpost2} alt="Profile" />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </p>
-                  <p className="card-text">
-                    <small className="text-muted">
-                      Last updated 3 mins ago
-                    </small>
-                  </p>
-                </div>
-              </div>
+      <>
+        <div className="post-container ">
+          <div class="card mt-5">
+            <a class="card-link">Username</a>
+            <img src={blogpost} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <label className="like">
+                <input type="checkbox" />
+                <div className="hearth" />
+              </label>
+              <i class="bi bi-save"></i>
             </div>
           </div>
         </div>
-      </div>
+        <div className="post-container ">
+          <div class="card mt-5">
+            <a class="card-link">Username</a>
+            <img src={blogpost} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <label className="like">
+                <input type="checkbox" />
+                <div className="hearth" />
+              </label>
+              <i class="bi bi-save"></i>
+            </div>
+          </div>
+        </div>
+        <div className="post-container ">
+          <div class="card mt-5">
+            <a class="card-link">Username</a>
+            <img src={blogpost} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <label className="like">
+                <input type="checkbox" />
+                <div className="hearth" />
+              </label>
+              <i class="bi bi-save"></i>
+            </div>
+          </div>
+        </div>
+        <div className="post-container ">
+          <div class="card mt-5">
+            <a class="card-link">Username</a>
+            <img src={blogpost} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <label className="like">
+                <input type="checkbox" />
+                <div className="hearth" />
+              </label>
+              <i class="bi bi-save"></i>
+            </div>
+          </div>
+        </div>
+        <div className="post-container ">
+          <div class="card mt-5">
+            <a class="card-link">Username</a>
+            <img src={blogpost} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <label className="like">
+                <input type="checkbox" />
+                <div className="hearth" />
+              </label>
+              <i class="bi bi-save"></i>
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
